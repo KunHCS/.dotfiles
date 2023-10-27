85e49147c85cd0ebf39b583f4df5e3ca5c3e2fec
Martin <kun.he.martin@gmail.com>
2023-10-27 01:35:20 -0700

    add keybind for code action for lsp

--- nvim/.config/nvim/lua/core/plugins/mason.lua
+++ nvim/.config/nvim/lua/core/plugins/mason.lua
@@ -7,23 +7,34 @@ local plugin = {
   }
 }
 
-plugin.config = function()
+-- keymaps --
+local on_attach = function(_, bufnr)
+  local keymap = vim.keymap -- for conciseness
+  local opts = { noremap = true, silent = true }
+  opts.buffer = bufnr
 
+  opts.desc = "See available code actions"
+  keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+end
+
+plugin.config = function()
   require("mason").setup()
   require("mason-lspconfig").setup{
     ensure_installed = {"lua_ls", "yamlls"}
   }
-
   local capabilities = require("cmp_nvim_lsp").default_capabilities()
   local lspconfig = require("lspconfig")
 
   -- yaml --
   lspconfig.yamlls.setup{
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach
   }
   -- lua --
   lspconfig.lua_ls.setup{
     capabilities = capabilities,
+    on_attach = on_attach,
     settings = { -- custom settings for lua
       Lua = {
         -- make the language server recognize "vim" global
